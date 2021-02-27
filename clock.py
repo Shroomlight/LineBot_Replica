2ddc14b1efa1213a3bd1b4ff92597235189cfc37
Shroomlight <a612348@yahoo.com.tw>
2021-02-27 12:17:35 +0800

    test clock.py

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import urllib.request as request
 
 clock = BlockingScheduler()
 
-@clock.scheduled_job('cron', day_of_week='0-6', hour='5-22', minute='*/20')
+@clock.scheduled_job('cron', day_of_week='0-6', hour='5-22', minute='*/2')
 def scheduled_job():
     url = "https://replicabot.herokuapp.com/"
     conn = request.urlopen(url)
