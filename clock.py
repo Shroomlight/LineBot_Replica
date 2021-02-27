a665dd5a2e838f0d954d3344a0c012534e3e3fe6
Shroomlight <a612348@yahoo.com.tw>
2021-02-27 13:28:01 +0800

    test clock.py' Time

--- clock.py
+++ clock.py
@@ -1,14 +1,24 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import urllib.request as request
+import datetime
 
 clock = BlockingScheduler()
 
 @clock.scheduled_job('cron', day_of_week='0-6', hour='5-22', minute='*/2')
 def scheduled_job():
-    url = "https://replicabot.herokuapp.com/"
-    conn = request.urlopen(url)
-        
-    for key, value in conn.getheaders():
-        print(key, value)
+    print("\n"+"===== APScheduler ====="+"\n")
+    print("clock.py runs every AM:6.00 to PM:11.00 */2")
+    print("Current time："f'{datetime.datetime.now().ctime()}'"\n")
+    print("===== APScheduler =====")
+
+    try:
+        url = "https://replicabot.herokuapp.com/"
+        conn = request.urlopen(url)
+        servertime=conn.getheaders()[2][1]
+        print("ServerTime："+servertime)
+        print("Success")
+
+    except:
+        print("Error！")
 
 clock.start()
