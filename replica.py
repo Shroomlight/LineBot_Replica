3c8813b27ed5e5df397c7f94c6a78424990356dc
BlueSphere <78530389+Shroomlight@users.noreply.github.com>
2021-05-12 14:07:08 +0800

    Update replica.py

--- replica.py
+++ replica.py
@@ -21,8 +21,8 @@ import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('gd8r9kQxyrGZrb4phBaA57fHMWY/P9jbMlX5O7REEYn9z3og6TvYtX5WyENXYOKBx0BL3LmumGNaTkOfSpNakhz+aqsd9d2vUCr7SbFH1saibQFwGOO0KHQu5JnAJNAqQYYZp+DVfVwj5Q22+OHY7AdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('ee758ba343a9fd7603ad160831a25a4f')
+line_bot_api = LineBotApi('')
+handler = WebhookHandler('')
 
 # 訪問clock.html
 @app.route("/")
